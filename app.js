c4b8b1757f36163e8a11f73f3306075dfe713921
Abhishek <apabhishek2002@gmail.com>
2021-05-29 16:22:00 +0530

    on progress

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const PORT = 3000;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+detials = {};
+io.on("connection", (socket) => {
+  //   console.log(" user is connected");
+  socket.on("login", (username) => {
+    console.log(`${username} is connected !`);
+    detials[socket.id] = username;
+    console.log(detials);
+  });
+  socket.on("user chat", (msg, username) => {
+    socket.broadcast.emit("user msg", msg, username);
+    console.log(`${username} : ${msg}`);
+  });
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  socket.on("disconnect", () => {
+    console.log(`${detials[socket.id]} is disconnected`);
+  });
+});
+
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- public/script/script.js
+++ public/script/script.js
@@ -1,21 +1,62 @@
+var socket = io();
+
 const input = document.querySelector(".input-cls");
 const btn = document.querySelector("button");
 const msgContainer = document.querySelector(".msg");
+const incomingMsg = document.querySelector(".incoming-msg");
+const outgoingMsg = document.querySelector(".outgoing-msg");
 const outgoingUl = document.querySelector(".outgoing-ul");
+const incomingUl = document.querySelector(".incoming-ul");
+var username = window.prompt("username");
+// when the user connects
+socket.emit("login", username);
+
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
+function recieveMsg(msg, username) {
+  var li = document.createElement("li");
+  var span = document.createElement("span");
+  var div = document.createElement("div");
+  var div2 = document.createElement("div");
+  var ul = document.createElement("ul");
+
+  li.textContent = msg;
+  span.textContent = username;
+
+  div2.classList.add("person-msg");
+  ul.classList.add("incoming-msg");
+  div2.appendChild(span);
+  div2.appendChild(li);
+  ul.appendChild(div2);
+  div.appendChild(ul);
+  msgContainer.appendChild(div);
+  msgContainer.scrollTop = msgContainer.scrollHeight;
+}
 function sendMsg() {
   if (input.value) {
     var msg = input.value;
+    // event send msg form client side to server
+    socket.emit("user chat", msg, username);
+
     var li = document.createElement("li");
+    var div = document.createElement("div");
+    var ul = document.createElement("ul");
+    div.classList.add("outgoing-msg");
+    ul.classList.add("outgoing-ul");
     li.textContent = msg;
-    outgoingUl.appendChild(li);
+    ul.appendChild(li);
+    div.appendChild(ul);
+    msgContainer.appendChild(div);
     msgContainer.scrollTop = msgContainer.scrollHeight;
     input.value = "";
   }
 }
+socket.on("user msg", (msg, username) => {
+  console.log("messge recieved by " + username);
+  recieveMsg(msg, username);
+});
 
 btn.addEventListener("click", sendMsg);
 
