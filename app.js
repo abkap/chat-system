cabeaff823174c1cee37fc416fc2899de3e2f90f
Abhishek <apabhishek2002@gmail.com>
2021-05-29 12:06:39 +0530

    backend

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
