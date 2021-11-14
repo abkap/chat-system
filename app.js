afd35d1813d2d1b5db885bb8caad13a6ed67fd8e
Abhishek <apabhishek2002@gmail.com>
2021-11-14 12:10:26 +0530

    choose port from env variable or use fixed one as 3000 (made for heroku)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
